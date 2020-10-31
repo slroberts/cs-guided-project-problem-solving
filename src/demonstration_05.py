62ba75545f3f01434cc550b6c326765a1f84c9ca
Shomari Roberts <shomariroberts@gmail.com>
2020-10-31 13:41:22 -0400

    completed demonstration 5

--- src/demonstration_05.py
+++ src/demonstration_05.py
@@ -21,6 +21,30 @@ Examples:
 Notes:
 - Return the name of the data type as a lowercase string.
 """
-def data_type(value):
-    # Your code here
+import datetime
+from datetime import date
 
+def data_type(value):
+    """
+    a function that returns the data type of a given argument.
+    """
+    
+    if type(value) == list:
+        return "list"
+    elif type(value) == dict:
+        return "dictionary"
+    elif type(value) == str:
+        return "string"
+    elif type(value) == int:
+        return "integer"
+    elif type(value) == float:
+        return "float"
+    elif type(value) == bool:
+        return "boolean"
+    elif type(value) == date:
+        return "date"
+
+print(data_type([1, 2, 3, 4])) #  ➞ "list"
+print(data_type({'key': "value"})) #  ➞ "dictionary"
+print(data_type("This is an example string.")) #  ➞ "string"
+print(data_type(datetime.date(2018,1,1))) #  ➞ "date" 
