51e1827ec17d77d46ae66f460e0953c2667e34df
Shomari Roberts <shomariroberts@gmail.com>
2020-10-31 21:54:22 -0400

    completed demonstration 8

--- src/demonstration_08.py
+++ src/demonstration_08.py
@@ -8,8 +8,19 @@ Examples:
 - parity(0) -> "Even"
 - parity(1) -> "Odd"
 - parity(2) -> "Even"
+
+# check if the input_int has a remainder. if input_int % 2 == 0 input_int "Even" else "Odd"
+
 """
 def parity(input_int):
-    # Your code here
+    """
+    a function that returns "Even" for even integers and "Odd" for odd integers.
+    """
+    if input_int % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
 
-
+print(parity(0)) # -> "Even"
+print(parity(1)) # -> "Odd"
+print(parity(2)) # -> "Even"
