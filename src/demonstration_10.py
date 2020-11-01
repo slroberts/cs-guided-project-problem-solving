c0cff89a370d744558cbad20b555c189fe18166a
Shomari Roberts <shomariroberts@gmail.com>
2020-10-31 23:35:52 -0400

    completed demonstration 10

--- src/demonstration_10.py
+++ src/demonstration_10.py
@@ -16,6 +16,21 @@ Notes:
 the maximum number is first.
 """
 def max_and_min(input_str):
-    # Your code here
+    """
+    a function that returns the maximum and minimum integers.
+    """
 
+    # convert input_str to list 
+    int_list = list(input_str.split(' ') )
+    
+    # find max and min with max() and min()
+    max_integer =  max(int_list)
+    min_integer =  min(int_list)
 
+    # concatenate max_integer and min_integer
+    return  max_integer + " " + min_integer
+     
+
+print(max_and_min("1 2 3 4 5")) # -> "5 1"
+print(max_and_min("1 2 -3 4 5")) #  -> "5 -3"
+print(max_and_min("1 9 3 4 -5")) # -> "9 -5"
